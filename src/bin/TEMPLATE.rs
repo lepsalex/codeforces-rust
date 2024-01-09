74f12df85c0978b76a80e45d3334f786d0d066c2
Alex Lepsa <lepsalex@gmail.com>
2024-01-09 15:49:02 -0500

    basic input scanner + template file

---
+++ src/bin/TEMPLATE.rs
@@ -0,0 +1,18 @@
+use input::Scanner;
+
+fn main() {
+    let mut input = Scanner::new();
+    solve()
+}
+
+fn solve() {}
+
+#[cfg(test)]
+mod test {
+    use crate::solve;
+
+    #[test]
+    fn case_1() {
+        solve();
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::{
+    collections::VecDeque,
+    fmt,
+    io::{self, BufRead},
+    str::FromStr,
+};
+
+pub struct Scanner {
+    tokens: VecDeque<String>,
+}
+
+impl Scanner {
+    pub fn new() -> Self {
+        let stdin = io::stdin();
+        let mut tokens = VecDeque::new();
+        for line in stdin.lock().lines() {
+            for token in line.unwrap().split_ascii_whitespace() {
+                tokens.push_back(token.to_owned());
+            }
+        }
+        Self { tokens }
+    }
+
+    pub fn next<T: FromStr>(&mut self) -> T
+        where
+            <T as FromStr>::Err: fmt::Debug,
+    {
+        T::from_str(&self.tokens.pop_front().unwrap()).unwrap()
+    }
+}
